d649d82ee69cbb6e8f4659fe0b37841b1f9cdf92
Artem <reutskyi.artem@lll.kpi.ua>
2026-05-24 23:20:21 +0300

    add terraform config

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -1,115 +1,54 @@
 terraform {
   required_providers {
-    libvirt = {
-      source  = "dmacvicar/libvirt"
-      version = "~> 0.7"
+    virtualbox = {
+      source  = "terra-farm/virtualbox"
+      version = "0.2.2-alpha.1"
     }
   }
 }
 
-provider "libvirt" {
-  uri = "qemu:///system"
-}
+resource "virtualbox_vm" "db" {
+  count  = 1
+  name   = "db-vm"
+  image  = "./virtualbox.box"
+  cpus   = 1
+  memory = "1024 mib"
 
-resource "libvirt_pool" "default" {
-  name = "simple-inventory"
-  type = "dir"
-  path = "/tmp/simple-inventory-pool"
-}
-
-resource "libvirt_volume" "base" {
-  name   = "ubuntu-base.img"
-  pool   = libvirt_pool.default.name
-  source = var.base_image
-  format = "qcow2"
-}
-
-resource "libvirt_volume" "worker" {
-  name           = "worker.img"
-  pool           = libvirt_pool.default.name
-  base_volume_id = libvirt_volume.base.id
-  format         = "qcow2"
-  size           = 10737418240 # 10GB
-}
-
-resource "libvirt_volume" "db" {
-  name           = "db.img"
-  pool           = libvirt_pool.default.name
-  base_volume_id = libvirt_volume.base.id
-  format         = "qcow2"
-  size           = 10737418240 # 10GB
-}
-
-resource "libvirt_network" "inventory_net" {
-  name      = "inventory-net"
-  mode      = "nat"
-  addresses = ["192.168.100.0/24"]
-  dhcp { enabled = true }
-  dns  { enabled = true }
-}
-
-resource "libvirt_cloudinit_disk" "worker_init" {
-  name      = "worker-init.iso"
-  pool      = libvirt_pool.default.name
-  user_data = file("${path.module}/cloud-init-worker.yml")
-}
-
-resource "libvirt_cloudinit_disk" "db_init" {
-  name      = "db-init.iso"
-  pool      = libvirt_pool.default.name
-  user_data = file("${path.module}/cloud-init-db.yml")
-}
-
-resource "libvirt_domain" "worker" {
-  name   = "worker"
-  memory = "2048"
-  vcpu   = 2
-
-  cloudinit = libvirt_cloudinit_disk.worker_init.id
-
-  network_interface {
-    network_id     = libvirt_network.inventory_net.id
-    wait_for_lease = true
+  network_adapter {
+    type = "nat"
   }
 
-  disk {
-    volume_id = libvirt_volume.worker.id
+  network_adapter {
+    type           = "hostonly"
+    host_interface = "VirtualBox Host-Only Ethernet Adapter"
   }
 
-  console {
-    type        = "pty"
-    target_type = "serial"
-    target_port = "0"
-  }
+  user_data = file("${path.module}/user_data.yml")
 }
 
-resource "libvirt_domain" "db" {
-  name   = "db"
-  memory = "1024"
-  vcpu   = 1
+resource "virtualbox_vm" "worker" {
+  count  = 1
+  name   = "worker-vm"
+  image  = "./virtualbox.box"
+  cpus   = 1
+  memory = "1024 mib"
 
-  cloudinit = libvirt_cloudinit_disk.db_init.id
-
-  network_interface {
-    network_id     = libvirt_network.inventory_net.id
-    wait_for_lease = true
+  network_adapter {
+    type = "nat"
   }
 
-  disk {
-    volume_id = libvirt_volume.db.id
+  network_adapter {
+    type           = "hostonly"
+    host_interface = "VirtualBox Host-Only Ethernet Adapter"
   }
 
-  console {
-    type        = "pty"
-    target_type = "serial"
-    target_port = "0"
-  }
-}
-
-output "worker_ip" {
-  value = libvirt_domain.worker.network_interface[0].addresses[0]
+  user_data = file("${path.module}/user_data.yml")
 }
 
 output "db_ip" {
-  value = libvirt_domain.db.network_interface[0].addresses[0]
+  value = element(virtualbox_vm.db.*.network_adapter.0.ipv4_address, 1)
+}
+
+output "worker_ip" {
+  value = element(virtualbox_vm.worker.*.network_adapter.0.ipv4_address, 1)
 }
