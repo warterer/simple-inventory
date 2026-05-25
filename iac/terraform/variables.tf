f4c07229c6f0223018d825c02fadc53a602400fb
Artem <reutskyi.artem@lll.kpi.ua>
2026-05-26 00:01:58 +0300

    remove unused cloud-init files

--- iac/terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "base_image" {
-  description = "Path to Ubuntu cloud image"
-  default     = "/home/artem/jammy-server-cloudimg-amd64.img"
-}
-
-variable "ansible_public_key" {
-  description = "SSH public key for ansible user"
-  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBBf+v5WQG7y/iA0umP346ts0YmriFbKG3FnkyW2r0B2 artem@LAPTOP-PLRK786C"
-}
